475133bc02ce15c27b4bbe4855632387568b911e
CheezItMan <chris@adadevelopersacademy.org>
2018-06-02 16:55:22 -0700

    add validation tests

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe Board do
-  let(:board) { Board.new }
+  let(:board) { boards(:adas) }
 
   it "must be valid" do
     value(board).must_be :valid?
+  end
+
+  it "is not valid without a name" do
+    board.name = nil
+
+    expect(board.valid?).to_be_falsy
   end
 end
