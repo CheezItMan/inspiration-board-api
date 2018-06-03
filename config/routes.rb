3c9a3e24c4cfacb707cc8c01b4cf86516712aa3b
CheezItMan <chris@adadevelopersacademy.org>
2018-06-02 20:17:50 -0700

    update routes for cards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :boards, except: [:edit, :new]
+  resources :boards, except: [:edit, :new] do
+    resources :cards, except: [:edit, :new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
